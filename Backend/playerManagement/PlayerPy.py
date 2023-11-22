882bb58cddfb32501de940a139c19a034aa7cbde
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-22 15:12:50 -0300

    setter de id

--- Backend/playerManagement/PlayerPy.py
+++ Backend/playerManagement/PlayerPy.py
@@ -1,19 +1,19 @@
 class PlayerPy:
     nome = ""
     playerId = 0
-    x = 0
-    y = 0
     hp = 100
     websocket = None
        
-    def __init__(self,nome, startXPos, startYPos, websocket):
+    def __init__(self,nome, websocket):
         self.nome = nome
-        self.x = startXPos
-        self.y = startYPos
         self.websocket = websocket
         
     def recebaDano(self, dano):
-        hp = hp - dano
+        self.hp = self.hp - dano
         
     def causeDano(self, PlayerPy, dano):
         PlayerPy.recebaDano(dano)
+        
+    def setId(self, id):
+        self.playerId = id
+        
