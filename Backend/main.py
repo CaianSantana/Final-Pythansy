df0c541cad7ebc2d2884b17f67b6b793da98b884
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-22 15:12:25 -0300

    mudancas parciais nao completamente funcionais

--- Backend/main.py
+++ Backend/main.py
@@ -1,33 +1,40 @@
 import asyncio 
 from websockets.server import serve
-from playerManagement.PlayerPy import PlayerPy 
+from playerManagement.PlayerPy import PlayerPy
+from game.Game import Game
 
-playersConections = set()
 funcoes = {}
+game:Game = None
 
 #Padrao de assinatura de toda funcao deve ser este a seguir {Negociavel de passar conexao algo a ser analisado}
-def join(conexao, playerName):
-    print("recebeu")
-    return PlayerPy(playerName, 0,0, conexao)
+def join(conexao, playerName)-> PlayerPy:
+    if(game.playerJaEstaEmPartida):    
+        player = PlayerPy(playerName, conexao)
+    return player
 
+def hit(conexao,message):
+    game.hit(message)
+
+
+
+#A conexao é passada como parametro
 async def echo(websocket):
-    print(funcoes)
-    
-    if(len(playersConections) > 2):
+    print(funcoes)    
+    if(game.limiteDeJogadoresAtingido()):
         return "Refused\nConexao lotada"
-    
     async for message in websocket:
         print(message)
-        playersConections.add(websocket)
         funcoes[message[0]](websocket,message)
         await websocket.send(message)
     return
     
 async def start():
+    game = Game()
     funcoes["J"] = join
+    funcoes["H"] = hit
     async with serve(echo, "localhost", 8080):
         print("Python ta on")
         await asyncio.Future()
         
+#inicia o loop de execucao
 asyncio.run(start())
-
