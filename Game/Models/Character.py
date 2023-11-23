e590f2b88216588bc0121ed3cea8fa94888004fa
CaianSantana <caian23@outlook.com>
2023-11-22 23:29:14 -0300

    states

--- Game/Models/Character.py
+++ Game/Models/Character.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.math import Vector2
 from Settings.Configuration import cellNumberX, cellSize
 from Models.Damage import Damage
-from Models.Side import Side
+from Models.States import States
 
 class Character():
     
@@ -19,9 +19,7 @@ class Character():
         self.speed = 1
         self.rect = self.pos
         self.target = None
-        self.side = self.getSide()
-        self.occupied = False
-        self.live = True
+        self.state = States.IDLE
         
     def draw(self):
         pass
@@ -29,44 +27,40 @@ class Character():
     def getInput(self):
         pass
     
-    def die(self):
-        self.live = False
-        colorImage = pygame.Surface(self.sprite.get_size()).convert_alpha()
-        colorImage.fill((47,79,79))
-        self.sprite.blit(colorImage, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
-        self.sprite = pygame.transform.rotate(self.sprite, 90) 
-    
     def defineTarget(self, target):
         if target != None:
-            if target.side != self.side:
-                self.target = target
-        else:
-            self.target = None
-    
-    def doBasicAttack(self):
-        print("Atacando o alvo para causar "+str(self.attack)+" de dano fisico")
-        self.target.receiveDamage(self.attack, Damage.PHYSICAL)
-        pass
-        
+            self.target = target
+            
     def update(self):
         self.getInput()
-        self.getSide()
         self.move()
+        self.doBasicAttack()
+    
+    def doBasicAttack(self):
+        if self.state == States.READY:
+            self.state = States.MARCHING
+        if self.state == States.MELEE:
+            print("Atacando o alvo para causar "+str(self.attack)+" de dano fisico")
+            self.target.receiveDamage(self.attack, Damage.PHYSICAL)
+            self.state = States.RETREATING
+        pass
+        
+    
         
     def doSomething(self):
         self.doBasicAttack()
     
     def isInOriginalPos(self):
         if self.pos.x == self.x and self.pos.y == self.y:
-            self.occupied = False
+            self.state = States.IDLE
         
     def move(self):
-        if self.target != None:
+        if self.state == States.MARCHING:
             if self.pos.x < self.target.x-1 or self.pos.x > self.target.x+1:
                 yRelation = self.pos.y - self.target.y
-                if self.side == Side.LEFT:
+                if self.x < cellNumberX/2:
                     self.pos.x+=1
-                elif self.side == Side.RIGHT:
+                elif self.x > cellNumberX/2:
                     self.pos.x-=1
                 if self.pos.x == cellNumberX/2:    
                     if yRelation>0:
@@ -74,13 +68,13 @@ class Character():
                     elif yRelation<0:
                         self.pos.y+=1
             else:
-                self.doSomething()
+                self.state = States.MELEE
                 self.defineTarget(None)
-        else:
+        elif self.state == States.RETREATING:
             if self.pos.x != self.x or self.pos.y != self.y:
-                if self.side == Side.LEFT:
+                if self.x < cellNumberX/2:
                     self.pos.x-=1
-                elif self.side == Side.RIGHT:
+                elif self.x > cellNumberX/2:
                     self.pos.x+=1
                 if self.pos.x == cellNumberX/2:
                     if self.pos.y < self.y:
@@ -89,11 +83,6 @@ class Character():
                         self.pos.y-=1
                 self.isInOriginalPos()
         
-    def getSide(self):
-        if self.x < cellNumberX/2:
-            self.side = Side.LEFT
-        else:
-            self.side = Side.RIGHT
     
     def receiveDamage(self, damage, type):
         if type == Damage.PHYSICAL:
@@ -107,3 +96,13 @@ class Character():
             print("Morreu.")
             self.die()
             pass
+    
+    def die(self):
+        self.state = States.DEAD
+        colorImage = pygame.Surface(self.sprite.get_size()).convert_alpha()
+        colorImage.fill((47,79,79))
+        self.sprite.blit(colorImage, (0,0), special_flags = pygame.BLEND_RGBA_MULT)
+        if self.x > cellNumberX/2:
+            self.sprite = pygame.transform.rotate(self.sprite, -90) 
+        else:
+            self.sprite = pygame.transform.rotate(self.sprite, 90) 
--- Game/Settings/Main.py
+++ Game/Settings/Main.py
@@ -4,23 +4,23 @@ from Settings.InputBox import InputBox
 from Models.Mage import Mage
 from Environments.Scenario import Scenario
 from Environments.Combat import Combat
+from Models.States import States
 
 #input = InputBox(570, 376, 140, 32)  -- Teste de inputbox
 
 class Main:
     
     def __init__(self):
-        self.playerChars = [Mage(4, 6)] #, Mage(2, 6)
-        self.enemychars = [Mage(11, 6)] #, Mage(13, 6)
+        self.playerChars = [Mage(4, 6), Mage(2, 7)] #, Mage(2, 6)
+        self.enemychars = [Mage(11, 6), Mage(13, 7)] #, Mage(13, 6)
         self.totalChars = self.playerChars + self.enemychars
-        self.totalChars[0].health = 2
-        self.totalChars[1].health = 2
+        self.totalChars[0].health = 4
+        self.totalChars[2].health = 4
         self.combat = Combat(self.playerChars, self.enemychars)
         self.combat.nextTurn()
         for char in self.enemychars:
             char.sprite = pygame.transform.flip(char.sprite, True, False) 
         self.cont = 0
-        self.contAction = 1
         self.scenario = Scenario("Forest")
         pass
     
@@ -31,6 +31,23 @@ class Main:
         pass
     def keyInput(self): 
         if self.combat.running == True:
+            if self.combat.turn.state==States.ACTING:
+                print("Turno de "+ str(self.combat.turn)+" iniciado")
+                nextItem = iter(self.combat.order)
+                for i in enumerate(self.totalChars):
+                    nextChar = self.combat.order.get(next(nextItem))
+                    print("Ainda não Passou")
+                    if self.combat.verifyTeam(self.combat.turn) and not self.combat.verifyTeam(nextChar) or not self.combat.verifyTeam(self.combat.turn) and self.combat.verifyTeam(nextChar):
+                        print("Passou")
+                        self.combat.turn.defineTarget(nextChar)
+                        self.combat.turn.state = States.READY
+                        break
+            elif self.combat.turn.state == States.IDLE:
+                self.combat.nextTurn()
+        else:
+            print("Combate finalizado.")
+        
+        """ if self.combat.running == True:
             if self.combat.turn.occupied and self.contAction > 0:
                 print("Turno de "+ str(self.combat.turn)+" iniciado")
                 nextItem = iter(self.combat.order)
@@ -44,7 +61,8 @@ class Main:
                 self.combat.nextTurn()
                 self.contAction = 1
         else:
-            print("Combate finalizado.")
+            print("Combate finalizado.")"""
+       
        
     def draw(self):
         self.scenario.draw()
