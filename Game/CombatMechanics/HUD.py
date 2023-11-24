4bccedab7f3f35406e6148aa77be5db44e979081
CaianSantana <caian23@outlook.com>
2023-11-23 22:08:50 -0300

    display className em HUD

--- Game/CombatMechanics/HUD.py
+++ Game/CombatMechanics/HUD.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.math import Vector2
 from Settings.Configuration import screen, screenHeight, screenWidth, cellNumberX
+from Models.Characters.Wizard import Wizard
 
 class HUD:
     def __init__(self, playerChars, gameFont):
@@ -9,7 +10,7 @@ class HUD:
         self.surface = pygame.Surface((screenWidth, self.height))
         self.playerChars = playerChars
         self.gameFont = gameFont
-        self.textStats = "Life  |  Mana"
+        self.textStats = "Character  |  Life  |  Mana"
         self.stats = []
         
         
@@ -21,19 +22,22 @@ class HUD:
         screen.blit(self.surface, (0, self.y))
         self.drawTeamStats()
         
-        
+      
+      
         
     def drawTeamStats(self):
         self.distance = 20
         textSurface = self.gameFont.render(self.textStats, False, (255, 255, 255))
-        textPos = Vector2((screenWidth*3/4), self.y+self.distance)
+        textPos = Vector2((screenWidth*4/6), self.y+self.distance)
         textRect = textSurface.get_rect(center = (textPos.x, textPos.y))
         screen.blit(textSurface, textRect)
         for char in self.playerChars:
+            if isinstance(char, Wizard):
+                className = "Wizard"
             self.distance+=40
-            text = str(char.health) + "    |   " + str(char.mana)
+            text = className +"    |    "+ str(char.health) + "     |   " + str(char.mana)
             statsSurface = self.gameFont.render(text, False, (255, 255, 255))
-            statPos = Vector2((screenWidth*3/4)-10, self.y+self.distance)
+            statPos = Vector2((screenWidth*4/6) , self.y+self.distance)
             statRect = statsSurface.get_rect(center = (statPos.x, statPos.y))
             screen.blit(statsSurface, statRect)
             
