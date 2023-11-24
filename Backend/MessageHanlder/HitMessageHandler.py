eeb6cb4f36f6c563594ac13e1ed2cc430b342a8c
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-24 12:55:21 -0300

    respassa na cadeia de comando para o CharMessageHandler

--- Backend/MessageHanlder/HitMessageHandler.py
+++ Backend/MessageHanlder/HitMessageHandler.py
@@ -1,7 +1,7 @@
 from websockets.server import serve
 from ResponseStrategy.HitResponse import HitResponse
 from ResponseStrategy.Response import Response
-
+from MessageHanlder.CharMessageHandler import CharMessageHandler
 
 class HitMessageHandler:
     game = None
@@ -16,16 +16,24 @@ class HitMessageHandler:
         
     async def canIawnser(self,message:str)-> bool:
         if(not message[0] == "H"):
+            nextMessageHandler = CharMessageHandler(self.game,self.websocket)
+            await nextMessageHandler.handleMessage(self.websocket,message)
             return False
         return True
     
     async def handleMessage(self, websocket,message:str):
         self.setWebSocket(websocket)
         if(await self.canIawnser(message)):
-            self.game.hit(message)
+            self.parseMessage(message)
             await self.sendResponse(HitResponse(int(message[7]),int(message[13])))
         pass
         
     async def sendResponse(self, response:Response):
         await response.messageSender(self.webSocket)
+        
+    def parseMessage(self,message):
+            print("how?")
+            print(message)
+            print("message?")
+            self.game.hit(0,0,0,0,10)
         
