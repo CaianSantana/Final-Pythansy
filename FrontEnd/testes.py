afeab76f8a3cd978b9077f1767b4a3e35b33af65
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-25 17:18:17 -0300

    enviando em json

--- FrontEnd/testes.py
+++ FrontEnd/testes.py
@@ -2,21 +2,40 @@
 
 import asyncio
 from websockets.sync.client import connect
+import json
 
 async def hello():
     with connect("ws://localhost:8080") as websocket:
-        websocket.send("J receba")
+        joinMessage = {
+                "action":"Join",
+                "nome":"Yasmin yaz bollaz"
+            }
+        websocket.send((json.dumps(joinMessage)))
         message = websocket.recv()
-        websocket.send("C id:0 hp:25 attack:30 ability:12 armor:10 magicResistance:15 speed:35 mana:20")
+        print(message)
+        charMessage = {
+                "action":"char",
+                "class":"empty",
+                "playerId":"0",
+                "hp":"25",
+                "attack":"30",
+                "ability":"12",
+                "armor":"10",
+                "magicResistance":"15",
+                "speed":"35",
+                "mana":"20"
+        }
+        websocket.send(json.dumps(charMessage))
         message = websocket.recv()
+        print(message)
         
-        while(True):
-            print(f"Received: {message}")
-            print()
-            websocket.send("H from:0\n to:0\n Dano:10\n")
-            message = websocket.recv()
-            print(f"Received: {message}")
-            await asyncio.sleep(1)
+        # while(True):
+        #     print(f"Received: {message}")
+        #     print()
+        #     websocket.send("H from:0 with:0 to:0 Dano:+10")
+        #     message = websocket.recv()
+        #     print(f"Received: {message}")
+        #     await asyncio.sleep(1.5)
         
 asyncio.get_event_loop().run_until_complete(hello())
         
