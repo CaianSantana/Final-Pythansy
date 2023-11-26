ee6dbfa3b52dfac7a9cc64d2552d1da3be85fb93
Ntheus <matheusoliveira.7313@gmail.com>
2023-11-25 21:49:23 -0300

    iniciando thread do jogo principal

--- Game/App.py
+++ Game/App.py
@@ -1,19 +1,30 @@
+import threading
+import asyncio
 from Settings.Configuration import pygame, clock, SCREEN_UPDATE
 from Main import Main
-
+from API.MessageListener import MessageListener
 pygame.init()
-
 mainGame = Main()
+messageListener:MessageListener = MessageListener(mainGame)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            mainGame.gameOver()
-        if event.type == SCREEN_UPDATE:
-            mainGame.update()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mainGame.keyInput(event)
-    mainGame.draw()  
-    pygame.display.update()
-    clock.tick(60)   
-    
+def serverStart(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(messageListener.listen())
+
+def start():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                mainGame.gameOver()
+            if event.type == SCREEN_UPDATE:
+                mainGame.update()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mainGame.keyInput(event)
+            mainGame.draw()  
+            pygame.display.update()
+            clock.tick(60)   
+
+loop = asyncio.get_event_loop()
+thread = threading.Thread(target=serverStart,args=(loop,))
+thread.start()
+start()
